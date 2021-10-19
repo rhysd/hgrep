3e8a19fb18ef44f17322c454c11ddbe435b75197
rhysd <lin90162@yahoo.co.jp>
2021-10-19 19:28:27 +0900

    add aarch64 support for Homebrew formula

--- HomebrewFormula/update.bash
+++ HomebrewFormula/update.bash
@@ -15,10 +15,14 @@ fi
 
 case "$OSTYPE" in
     darwin*|freebsd*)
-        i_opt="-i ''"
+        _sed() {
+            sed -i '' "$@"
+        }
     ;;
     *)
-        i_opt="-i"
+        _sed() {
+            sed -i "$@"
+        }
     ;;
 esac
 
@@ -34,16 +38,30 @@ fi
 
 echo "Update formula to version ${VERSION}"
 
-MAC_ZIP="hgrep-${VERSION}-x86_64-apple-darwin.zip"
-MAC_URL="https://github.com/rhysd/hgrep/releases/download/${VERSION}/${MAC_ZIP}"
 
-echo "Downloading ${MAC_ZIP}..."
-curl -LO "$MAC_URL"
-MAC_SHA="$(shasum -a 256 "$MAC_ZIP" | cut -f 1 -d ' ')"
-echo "Mac sha256: ${MAC_SHA}"
-sed $i_opt -E "s/    sha256 '[0-9a-f]*' # mac/    sha256 '${MAC_SHA}' # mac/" hgrep.rb
+# macOS x86_64
+MAC_ZIP_X86_64="hgrep-${VERSION}-x86_64-apple-darwin.zip"
+MAC_URL_X86_64="https://github.com/rhysd/hgrep/releases/download/${VERSION}/${MAC_ZIP_X86_64}"
+
+echo "Downloading ${MAC_ZIP_X86_64}..."
+curl -LO "$MAC_URL_X86_64"
+MAC_SHA_X86_64="$(shasum -a 256 "$MAC_ZIP_X86_64" | cut -f 1 -d ' ')"
+echo "Mac x86_64 sha256: ${MAC_SHA_X86_64}"
+_sed -E "s/    sha256 '[0-9a-f]*' # mac_x86_64/    sha256 '${MAC_SHA_X86_64}' # mac_x86_64/" hgrep.rb
 
 
+# macOS aarch64
+MAC_ZIP_AARCH64="hgrep-${VERSION}-aarch64-darwin.zip"
+MAC_URL_AARCH64="https://github.com/rhysd/hgrep/releases/download/${VERSION}/${MAC_ZIP_AARCH64}"
+
+echo "Downloading ${MAC_ZIP_AARCH64}..."
+curl -LO "$MAC_URL_AARCH64"
+MAC_SHA_AARCH64="$(shasum -a 256 "$MAC_ZIP_AARCH64" | cut -f 1 -d ' ')"
+echo "Mac aarch64 sha256: ${MAC_SHA_AARCH64}"
+_sed -E "s/    sha256 '[0-9a-f]*' # mac_aarch64/    sha256 '${MAC_SHA_AARCH64}' # mac_aarch64/" hgrep.rb
+
+
+# Linux x86_64
 LINUX_ZIP="hgrep-${VERSION}-x86_64-unknown-linux-gnu.zip"
 LINUX_URL="https://github.com/rhysd/hgrep/releases/download/${VERSION}/${LINUX_ZIP}"
 
@@ -51,10 +69,10 @@ echo "Downloading ${LINUX_ZIP}..."
 curl -LO "$LINUX_URL"
 LINUX_SHA="$(shasum -a 256 "$LINUX_ZIP" | cut -f 1 -d ' ')"
 echo "Linux sha256: ${LINUX_SHA}"
-sed $i_opt -E "s/    sha256 '[0-9a-f]*' # linux/    sha256 '${LINUX_SHA}' # linux/" hgrep.rb
+_sed -E "s/    sha256 '[0-9a-f]*' # linux/    sha256 '${LINUX_SHA}' # linux/" hgrep.rb
 
 echo "Version: ${VERSION}"
-sed $i_opt -E "s/  version '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*'/  version '${VERSION#v}'/" hgrep.rb
+_sed -E "s/  version '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*'/  version '${VERSION#v}'/" hgrep.rb
 
 echo "Clean up zip files"
 rm -rf ./*.zip
