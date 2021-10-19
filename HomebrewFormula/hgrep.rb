a8ae7b42ba5f43b0891c530922546d8159a4b0f2
rhysd <lin90162@yahoo.co.jp>
2021-10-19 17:30:30 +0900

    update Homebrew formula

--- HomebrewFormula/hgrep.rb
+++ HomebrewFormula/hgrep.rb
@@ -1,14 +1,14 @@
 class Hgrep < Formula
-  version '0.1.2'
+  version '0.1.3'
   desc 'hgrep is grep with human-friendly search output'
   homepage 'https://github.com/rhysd/hgrep'
 
   if OS.mac?
     url "https://github.com/rhysd/hgrep/releases/download/v#{version}/hgrep-v#{version}-x86_64-apple-darwin.zip"
-    sha256 'e71ffc728bb6b3770b9d4f429aeb82a1466205c5e30840931bcea27a933e5502' # mac
+    sha256 'eb7649b0d7e44d6433445a8923beb48617e38fc768e72ad658c2900be07d7c6b' # mac
   elsif OS.linux?
     url "https://github.com/rhysd/hgrep/releases/download/v#{version}/hgrep-v#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 '76c365fcd09deced7f0c73bb2af8c9db3bdf5a7703984760f6b1ced02110b2d6' # linux
+    sha256 '3c920e02fc19804c2092782b680f47a46944e580e3dffe3ebcbedcf0e15b9021' # linux
   end
 
   def install
