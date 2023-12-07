a3399dfa82ab5219bf3557838670d239494b45f3
rhysd <lin90162@yahoo.co.jp>
2023-12-07 14:33:42 +0900

    replace dirs-next with dirs

--- src/bat.rs
+++ src/bat.rs
@@ -42,10 +42,10 @@ fn get_cache_dir() -> Option<PathBuf> {
     let dir = env::var_os("XDG_CACHE_HOME")
         .map(PathBuf::from)
         .filter(|p| p.is_absolute())
-        .or_else(|| dirs_next::home_dir().map(|d| d.join(".cache")));
+        .or_else(|| dirs::home_dir().map(|d| d.join(".cache")));
 
     #[cfg(not(target_os = "macos"))]
-    let dir = dirs_next::cache_dir();
+    let dir = dirs::cache_dir();
 
     dir.map(|d| d.join("bat"))
 }
